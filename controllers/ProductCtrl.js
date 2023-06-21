8ba8d9bfa5486c1888a740545657660e47627c4a
nakuldevkhajuria <nakuldevkhajuria@gmail.com>
2023-06-21 18:33:26 +0530

    updated the create Product and added the middlewares in these routes

--- controllers/ProductCtrl.js
+++ controllers/ProductCtrl.js
@@ -1,9 +1,16 @@
 const ProductModel = require("../models/ProductModel")
 const asyncHandler = require('express-async-handler')
+const slugify = require('slugify')
+
+
+
 
 const createProduct = asyncHandler(async(req,res)=>{
 
     try {
+        if(req.body.title){
+            req.body.slug = slugify(req.body.title)
+        }
         const newProduct = await ProductModel.create(req.body)
         res.json({ newProduct})
     } catch (error) {
@@ -12,6 +19,34 @@ const createProduct = asyncHandler(async(req,res)=>{
     
 
     
+})
+
+const updateProduct = asyncHandler(async(req,res)=>{
+    const {id} = req.params;
+    try {
+        if(req.body.title){
+            req.body.slug = slugify(req.body.title)
+        }
+        const updateProduct = await ProductModel.findByIdAndUpdate(id,req.body,{new:true})
+      
+        res.json(updateProduct)
+    } 
+    catch (error) {
+    throw new Error(error);    
+    }
+})
+
+const deleteProduct = asyncHandler(async(req,res)=>{
+    const {id} = req.params;
+    try {
+
+        const deleteProduct = await ProductModel.findByIdAndRemove(id)
+      
+        res.json(deleteProduct)
+    } 
+    catch (error) {
+    throw new Error(error);    
+    }
 })
 
 const getAProduct = asyncHandler(async(req,res)=>{
@@ -28,9 +63,10 @@ const getAProduct = asyncHandler(async(req,res)=>{
 })
 
 const getAllProduct = asyncHandler(async(req,res)=>{
+    // console.log(req.query)
     try {
 
-        const getAllProduct = await ProductModel.find()
+        const getAllProduct = await ProductModel.find(req.query)
         res.json({getAllProduct})
     } 
     catch (error) {
@@ -44,4 +80,6 @@ module.exports = {
     createProduct,
     getAProduct,
     getAllProduct,
+    updateProduct,
+    deleteProduct
 }
